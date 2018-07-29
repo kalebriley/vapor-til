0328a13fb6324415531f6294db20d4b8b1c5cf65
kaleb riley <kaleb@pink.io>
2018-07-28 20:03:46 -0600

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,10 +6,15 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello \(name)"
+    }
+    
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        return try req.content.decode(Acronym.self) .flatMap(to: Acronym.self) { arconym in
+            return arconym.save(on: req)
+        }
+    }
 }
