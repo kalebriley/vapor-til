1a93f21c91b0f0a1da99a165c6b46c8045b05fc6
kaleb riley <kaleb@pink.io>
2018-08-04 21:08:42 -0600

    Add CRUD operations

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -12,9 +13,45 @@ public func routes(_ router: Router) throws {
         return "Hello \(name)"
     }
     
+    // MARK: - Acronyms
+    router.get("api", "acronyms") { req -> Future<[Acronym]> in
+        return Acronym.query(on: req).all()
+    }
+    
+    router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+        return try req.parameters.next(Acronym.self)
+    }
+    
     router.post("api", "acronyms") { req -> Future<Acronym> in
         return try req.content.decode(Acronym.self) .flatMap(to: Acronym.self) { arconym in
             return arconym.save(on: req)
         }
+    }
+    
+    router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+        return try flatMap(to: Acronym.self, req.parameters.next(Acronym.self), req.content.decode(Acronym.self)) { acronym, updateAcronym in
+            acronym.short = updateAcronym.short
+            acronym.long = updateAcronym.long
+            return acronym.save(on: req)
+        }
+    }
+    
+    router.delete("api", "acronyms", Acronym.parameter) { req -> Future<HTTPStatus> in
+        return try req.parameters.next(Acronym.self).delete(on: req).transform(to: HTTPStatus.noContent)
+    }
+    
+    router.get("api", "acronyms", "sorted") { req -> Future<[Acronym]> in
+        return Acronym.query(on: req).sort(\.short, .ascending).all()
+    }
+    
+    router.get("api", "acronyms", "search") { req -> Future<[Acronym]> in
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        
+        return Acronym.query(on: req).group(.or) { or in
+            or.filter(\.short == searchTerm)
+            or.filter(\.long == searchTerm)
+        }.all()
     }
 }
